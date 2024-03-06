3fada80a772d200de35c22379cf551817c40e223
Abu10thahir7-github <142816135+Abu10thahir7-github@users.noreply.github.com>
2024-03-06 10:34:20 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import React from "react";
+import ReactDOM from "react-dom";
+import From from "./componet/from";
+function App() {
+  return (
+    <div className="App">
+      <From />
+    </div>
+  );
+}
+export default App;
